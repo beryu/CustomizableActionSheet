11eef92f126da9aba24357f37a550607e298592f
Ryuta Kibe <beryu@blk.jp>
2016-04-23 00:06:33 +0900

    Support Carthage

---
+++ CustomizableActionSheet/CustomizableActionSheet.h
@@ -0,0 +1,19 @@
+//
+//  CustomizableActionSheet.h
+//  CustomizableActionSheet
+//
+//  Created by beryu on 2016/04/22.
+//  Copyright © 2016年 blk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomizableActionSheet.
+FOUNDATION_EXPORT double CustomizableActionSheetVersionNumber;
+
+//! Project version string for CustomizableActionSheet.
+FOUNDATION_EXPORT const unsigned char CustomizableActionSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomizableActionSheet/PublicHeader.h>
+
+
