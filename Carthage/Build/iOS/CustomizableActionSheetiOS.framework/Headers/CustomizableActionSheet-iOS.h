ad9cc73a1484652618ec5ad9b159b1f44d19de37
Ryuta Kibe <beryu@blk.jp>
2016-04-23 00:21:27 +0900

    Support Carthage

---
+++ Carthage/Build/iOS/CustomizableActionSheetiOS.framework/Headers/CustomizableActionSheet-iOS.h
@@ -0,0 +1,19 @@
+//
+//  CustomizableActionSheet-iOS.h
+//  CustomizableActionSheet-iOS
+//
+//  Created by beryu on 2016/04/23.
+//  Copyright © 2016年 blk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomizableActionSheet-iOS.
+FOUNDATION_EXPORT double CustomizableActionSheet_iOSVersionNumber;
+
+//! Project version string for CustomizableActionSheet-iOS.
+FOUNDATION_EXPORT const unsigned char CustomizableActionSheet_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomizableActionSheet_iOS/PublicHeader.h>
+
+
--- CustomizableActionSheet/ViewController.swift
+++ CustomizableActionSheet/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CustomizableActionSheetiOS
 
 class ViewController: UIViewController {
   
